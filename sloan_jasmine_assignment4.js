21923d8dfd9d88efa9052ff92ad189fb31991673
Jasmine Jamieson <Jasmine@unknown28cfdaee4738.gateway.2wire.net>
2012-04-24 08:14:49 -0400

    added 1st string to repo

--- sloan_jasmine_assignment4.js
+++ sloan_jasmine_assignment4.js
@@ -1,5 +1,23 @@
+// Author: Jasmine Sloan
+// 04/22/2012
+// Week 4 Assignment
+// Libraries
+
+
 // String
 
 // Does a string follow a 123-456-7890 pattern like a phone number?
 
+var phoneNum = 123-456-7890
 
+var checkPhoneNum = function(phoneNumber){
+	checkPhone = "";
+	var number = /^\(?([0-9]{6})\)?[-. ]([0-9]{7})[-. ]([0-9]{8})$/;
+	
+	if(phoneNumber.test(number)){
+		return(checkPhone = "Phone number " + phoneNum + " is valid.");
+	}else{
+		return(checkPhone = "Phone number " + phoneNum + "is invalid.");
+	}
+};
+console.log("This is a valid phone number.")
